5f3d42310787d5ead954f012c2bad1a9f44babbe
Waldemar <waldemariturrieta.dev@gmail.com>
2026-03-31 15:05:12 -0300

    sobre mi

---
+++ resources/views/sobre-mi.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sobre mi</title>
+
+<style>
+body {
+font-family: Arial;
+background-color: #f4f4f4;
+margin: 40px;
+}
+
+.container {
+background: white;
+padding: 20px;
+border-radius: 10px;
+max-width: 600px;
+}
+
+h1 {
+color: #333;
+}
+
+p {
+line-height: 1.6;
+}
+</style>
+
+</head>
+
+<body>
+
+<center>
+<div class="container">
+    <h1>Sobre mi</h1>
+    <p><b>Nombre:</b> q paja wacho</p>
+    <p><b>Edad:</b> 52 dias</p>
+    <img src="https://images.squarespace-cdn.com/content/v1/58a13eba20099eb147e68d26/f5c88eae-b8c0-4eb5-9190-03c4ee6a11c2/zeiram-face.png" alt="Zeiram face">
+</div>
+</center>
+
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre-mi', function () {
+return view('sobre-mi');
+});
