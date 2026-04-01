ba501131893c2f3d21638063bdba4337ac59d952
Enzo <enzo-2304@hotmail.com>
2026-03-31 23:19:38 -0300

    Agregue algunos detalles

--- resources/views/exito.blade.php
+++ resources/views/exito.blade.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+</head>
 <body>
-        <p>Recibimos tu mensaje. Un asesor comercial se comunicará contigo a la
+         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+         {{--para modificar algo tiene ir todo dentro de class --}}
+        <p class="fs-4 fw-bolder text-success">Recibimos tu mensaje. Un asesor comercial se comunicará contigo a la
             brevedad. ¡Muchas gracias!</p>
     </body>
 </html>
