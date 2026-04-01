d4c6346683965780fcda1bfd9a7446a6f5070554
Enzo <enzo-2304@hotmail.com>
2026-04-01 14:11:02 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@ line-height: 1.6;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Welcome to Castlevania</title>
+    <title>Retro</title>
 </head>
 
     <body class="bg-dark text-white">
