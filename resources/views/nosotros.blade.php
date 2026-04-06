b6e9656a2933ca7f376749fca1ff547ae30326fc
Enzo <enzo-2304@hotmail.com>
2026-04-06 16:08:02 -0300

    .

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -12,14 +12,12 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         
-        <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 
-        <link rel="stylesheet" href="{{ asset('css/estilos.css?v=3') }}">
+        <link rel="stylesheet" href="{{ asset('css/estilos.css?v=7') }}">
     </head>
 
     <body class="bg-black text-white">
          <x-navbar />
-        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     
 
@@ -106,10 +104,56 @@
                         </div>
                     </div>
                 </div>
+
+                {{-- ACORDEADO  --}}
+
+                <div class="accordion" id="acordeonCatacumbas">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq1" aria-expanded="false" aria-controls="faq1">
+                                1. ¿Hacen envíos a todo el país?
+                            </button>
+                        </h2>
+                        <div id="faq1" class="accordion-collapse collapse" data-bs-parent="#acordeonCatacumbas">
+                            <div class="accordion-body">
+                                Sí, preparamos y despachamos paquetes todos los días. Hacemos envíos desde Corrientes a cualquier punto de Argentina a través de correo certificado para que tu consola llegue impecable.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq2" aria-expanded="false" aria-controls="faq2">
+                                2. ¿Los juegos y consolas usadas tienen garantía?
+                            </button>
+                        </h2>
+                        <div id="faq2" class="accordion-collapse collapse" data-bs-parent="#acordeonCatacumbas">
+                            <div class="accordion-body">
+                                ¡Por supuesto! Todo nuestro hardware pasa por un riguroso proceso de limpieza y mantenimiento. Ofrecemos 3 meses de garantía en consolas reacondicionadas y chipeadas.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq3" aria-expanded="false" aria-controls="faq3">
+                                3. ¿Compran consolas o juegos retro usados?
+                            </button>
+                        </h2>
+                        <div id="faq3" class="accordion-collapse collapse" data-bs-parent="#acordeonCatacumbas">
+                            <div class="accordion-body">
+                                Sí, tomamos tus consolas viejas (funcionen o no) como parte de pago o las compramos directamente para nuestro taller de restauración.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+            
         </section>
         
     <x-footer />
-        
+ 
     </body>
 </html>
