6e1ff27eddc39bca9c6a02c0b2d6afb82020b8e1
Enzo <enzo-2304@hotmail.com>
2026-04-11 12:06:25 -0300

    Update faq.blade.php

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -22,7 +22,8 @@
     <section class="py-5"> 
         <div class="container" style="width: 100%"> 
             <div class="row justify-content-center"> <div class="col-12 col-md-8 col-lg-6"> <div class="accordion" id="acordeonCatacumbas" style="--bs-accordion-btn-padding-y: 0.75rem;">
-                        <div class="accordion-item">
+                     <h2 class="font-weight-bold mb-4">DESPEJA TUS DUDAS</h2>
+                    <div class="accordion-item">
                             <h2 class="accordion-header">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq1" aria-expanded="false" aria-controls="faq1">
                                     1. ¿Hacen envíos a todo el país?
@@ -60,6 +61,46 @@
                                 </div>
                             </div>
                         </div>
+
+                        {{-- OTRA PREGUNTA --}}
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq4" aria-expanded="false" aria-controls="faq4">
+                                    4. ¿Cuáles son las formas de pago?
+                                </button>
+                            </h2>
+                            <div id="faq4" class="accordion-collapse collapse" data-bs-parent="#acordeonCatacumbas">
+                                <div class="accordion-body">
+                                    Aceptamos tarjetas de débito y crédito a través de Mercado Pago. Además, si elegís abonar por transferencia bancaria o efectivo, tenés un [10%] de descuento sobre el total de tu compra."
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq5" aria-expanded="false" aria-controls="faq5">
+                                    5. ¿Dónde está mi compra? 
+                                </button>
+                            </h2>
+                            <div id="faq5" class="accordion-collapse collapse" data-bs-parent="#acordeonCatacumbas">
+                                <div class="accordion-body">
+                                     <p class="mb-3">Hay varias razones posibles por las que un cliente de Catacumbas puede no haber recibido una compra: </p>
+                                    {{-- ol crea la lista numerada. ps-4 le da un poco de sangría a la izquierda --}}
+                                    <ol class="ps-4 mb-4" style="line-height: 1.8;">
+
+                                        <li class="mb-2">
+                                            <strong>La dirección de correo electrónico se escribió incorrectamente:</strong>Si escribiste mal tu mail al hacer el pedido,<a href="/contacto" class="text-danger text-decoration-none fw-bold">contactanos</a> y lo arreglaremos de inmediado. 
+                                        </li>
+
+                                        <li class="mb-2">
+                                            <strong>El pago no se acredito:</strong>
+                                        </li>
+                                    
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
                     </div> 
                 </div> 
             </div>
@@ -68,6 +109,6 @@
 
     <x-footer />
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+   
 </body>
 </html>
