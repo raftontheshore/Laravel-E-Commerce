e8cecb10f6b26c57307fa073d7d39936d4718931
Enzo <enzo-2304@hotmail.com>
2026-06-13 14:05:31 -0300

    responsive

--- resources/views/dashboard-usuarios.blade.php
+++ resources/views/dashboard-usuarios.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="Dashboard Catacumbas">
     <meta name="author" content="Enzo">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Catacumbas - Usuarios</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="{{ asset('adminDashBoard/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
